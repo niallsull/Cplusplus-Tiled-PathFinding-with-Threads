1558421c6958f956ddfccd38b39c8b764f724bcf
niallsull <niallsull@yahoo.co.uk>
2013-12-10 09:08:51 +0000

    Create CollideTile.cpp

---
+++ CollideTile.cpp
@@ -0,0 +1,16 @@
+#include "CollideTile.h"
+
+
+CollideTile::CollideTile(void)
+{
+}
+
+CollideTile::CollideTile(int cost,bool collide)
+{
+	cost=cost;
+	collidable=collide;
+}
+
+CollideTile::~CollideTile(void)
+{
+}
